918056e113955636fd34a422216da2ed24cd688b
Susan Winters <susanwinters214@gmail.com>
2023-03-28 10:05:28 -0500

    static keyword temp done

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace StaticExercise
+﻿using StaticExercise;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Fahrenheit TO Celsius: {TempConverter.FahrenheitToCelsius(32)}");
+         
+
+            Console.Write($"Celsius TO Fahrenheit: {TempConverter.CelsiusToFahrenheit(0)} ");
+          
         }
+
+
+       
+
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+           return (fTemp - 32)  *  5/9;
+        }
+
+
+        public static double CelsiusToFahrenheit(double cTemp)
+        {
+          return (cTemp * 9 / 5)  + 32;
+        }
+
+
+    }
+}
